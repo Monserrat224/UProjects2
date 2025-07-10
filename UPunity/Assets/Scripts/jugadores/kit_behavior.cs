3c3f1c5423e88fbac1477591eca1e5eb7583af5f
Monserrat224 <monserrat.delcristo@ucic.edu.mx>
2025-07-10 00:26:56 -0600

    v3.1

---
+++ UPunity/Assets/Scripts/jugadores/kit_behavior.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class kit_behavior : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed = 25f; // Velocidad ajustada para un movimiento rápido
+
+    [Header("References")]
+    public Animator animator;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        // Calcular desplazamiento (sin aceleración ni deceleración)
+        float movement = horizontalInput * moveSpeed * Time.deltaTime;
+
+        // Mover el personaje
+        transform.position += new Vector3(movement, 0, 0);
+
+        // Animación y flip del sprite
+        
+        if (horizontalInput != 0)
+        {
+            // Actualizar la animación con la velocidad (sin suavizado)
+
+            animator.SetFloat("movement", Mathf.Abs(horizontalInput)); // Siempre 1 o 0
+
+            // Voltear el sprite según la dirección
+            transform.localScale = new Vector3(Mathf.Sign(horizontalInput), 1, 1);
+        }
+        else
+        {
+            // Detener la animación si no hay input
+            animator.SetFloat("movement", 0);
+        }
+    }
+
+}
